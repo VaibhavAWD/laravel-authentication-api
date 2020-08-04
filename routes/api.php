71a4b7e0eadd8d2f7df59ecf93d3250d5368a00a
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2020-08-05 01:34:04 +0530

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function() {
+
+    Route::prefix('auth')->group(function() {
+
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login');
+
+    });
+
+    Route::group([
+        "middleware" => 'auth:api'
+    ], function() {
+
+        Route::get('hello', 'AuthController@hello');
+
+    });
+
 });
