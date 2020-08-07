70221ae3fb9e7307b3778a2962cbc253ce3aa759
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2020-08-07 20:10:29 +0530

    Register User

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,7 +10,24 @@ class AuthController extends Controller
 {
     
     public function register(Request $request) {
-        echo "Register Endpoint Requested";
+        
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|string|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
+
+        $user = new User([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => \bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            "message" => "User registered successfully!"
+        ]);
     }
 
     public function login() {
